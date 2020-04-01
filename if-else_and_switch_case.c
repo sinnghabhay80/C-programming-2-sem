05884a96997f61ccf93cb423ee3faeadcc4fefb4
sinnghabhay80 <55084281+sinnghabhay80@users.noreply.github.com>
2020-04-01 17:48:08 +0530

    Update if-else_and_switch_case.c

--- if-else_and_switch_case.c
+++ if-else_and_switch_case.c
@@ -838,22 +838,3 @@ scanf("%c",&a);
 getch();
 }
 
-/*#27 program to print reverse a given number.*/
-
-#include<stdio.h>
-#include<conio.h>
-int main()
-{
-int n,d,s=0;
-printf("Enter the number");
-scanf("%d",&n);
-while(n>0)
-{
-d=n%10;
-s=s*10+d;
-n=n/10;
-}
-printf("The reverse is %d",s);
-getch();
-}
-
